b114248eac282262225ab0f3003c474d2be6b951
sfish37699 <53944516+sfish37699@users.noreply.github.com>
2019-08-21 17:11:54 +0100

    Adding a few lines to show GIT works

--- HW1_CPP_GH/HW1_CPP_GH.cpp
+++ HW1_CPP_GH/HW1_CPP_GH.cpp
@@ -6,6 +6,8 @@
 int main()
 {
     std::cout << "Hello World!\n";
+	std::cout << "This is under GitHub version management\n";
+	std::cout << "And another line....\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
